a8e8d43ca5d08f1db677519b374ee20fcbae3213
somsuk kounsaweng <somsukkounsaweng@gmail.com>
2021-06-16 21:26:20 +0700

    test

---
+++ src/main/java/com/learn/demo/HelloController.java
@@ -0,0 +1,16 @@
+package com.learn.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/hello")
+public class HelloController {
+
+    @GetMapping
+    public String hello() {
+        return "Hello";
+    }
+
+}
